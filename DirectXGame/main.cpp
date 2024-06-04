7c7fbcbb06a8e38876e3bc36266cc455ffbc14cc
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-06-04 17:48:46 +0900

    ブロック描写

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -94,9 +94,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	}
 
 	// 各種解放
-	delete gameScene;
-	// 3Dモデル解放
-	Model::StaticFinalize();
+//	SafeDelete(gameScene);
 	audio->Finalize();
 	// ImGui解放
 	imguiManager->Finalize();
