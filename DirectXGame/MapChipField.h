4bc1c142198b116c1bb7d72e43719ea0510d723f
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-06-13 12:47:25 +0900

    少しやった

---
+++ DirectXGame/MapChipField.h
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+
+
+class MapChipField {
+
+static inline const float kBlockWidth = 1.0f;
+
+static inline const float kBlockHeight = 1.0f;
+
+
+static inline const uint32_t kNumBlockVirtical = 20;
+
+static inline const uint32_t kNumBlockHorizontal = 100;
+
+MapChipData mapChipData_;
+
+
+};
+
+enum class MAochipType {
+
+	kBlack,
+	kBlock,
+
+
+
+};
+
+
+struct MapChipData {
+
+std::vector<std::vector<MapChipType>> data;
+
+};
