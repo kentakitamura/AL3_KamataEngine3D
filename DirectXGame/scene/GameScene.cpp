41b66c1dc858421a0b3f01fb0d9c2af214dee59b
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-07-05 11:31:44 +0900

    完了

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -30,7 +30,7 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 
 	// ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("block.png");
+	//textureHandle_ = TextureManager::Load("block.png");
 	// 3Dモデルの生成
 	model_ = Model::Create();
 	modelBlock_ = Model::Create();
