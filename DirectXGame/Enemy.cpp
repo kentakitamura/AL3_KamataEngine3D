c901963558e61a94c9efb218cd9b9b96b1b328e9
kentakitamura <146901475+kentakitamura@users.noreply.github.com>
2024-07-16 13:12:55 +0900

    敵を描画させるところ

---
+++ DirectXGame/Enemy.cpp
@@ -0,0 +1,16 @@
+﻿#include "Enemy.h"
+
+
+void Enemy::Initialize(Model* model, ViewProjection* viewProjection) {
+
+	worldTransform_.Initialize();
+	model_ = model;
+	viewProjection_ = viewProjection;
+}
+
+void Enemy::Update() {}
+
+void Enemy::Draw() {
+	// 3Dモデル描画
+	model_->Draw(worldTransform_, *viewProjection_);
+}
---
+++ DirectXGame/Enemy.h
@@ -0,0 +1,29 @@
+﻿#include "Model.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+class Enemy {
+	
+
+	public:
+	/// 初期化
+	
+	
+	void Initialize(Model* model, ViewProjection* viewProjection);
+
+	/// 更新
+	void Update();
+
+	/// 描画
+
+	void Draw();
+  
+
+  private:
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	ViewProjection* viewProjection_ = nullptr;
+	// モデル
+	Model* model_ = nullptr;
+
+};
